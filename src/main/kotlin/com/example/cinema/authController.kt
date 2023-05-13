8d859db82dcef90ef732d5a15f0dcc1a0ca627d7
soollyy <solianatesema1@gmail.com>
2023-05-14 00:49:40 +0100

    error messages for incorrect username or password and JWT is returned once user logs in successfully

--- src/main/kotlin/com/example/cinema/authController.kt
+++ src/main/kotlin/com/example/cinema/authController.kt
@@ -5,11 +5,17 @@ import com.example.cinema.dtos.Message
 import com.example.cinema.dtos.RegisterDTO
 import com.example.cinema.models.Client
 import com.example.cinema.services.ClientService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
+//import org.springframework.boot.web.server.Cookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("api")
@@ -26,7 +32,7 @@ class authController (private val clientService: ClientService){
     }
 
     @PostMapping("login")
-    fun login(@RequestBody body: LoginDTO): ResponseEntity<Any>{
+    fun login(@RequestBody body: LoginDTO, response: HttpServletResponse): ResponseEntity<Any>{
         val client = this.clientService.findByEmail(body.email)
             ?: return ResponseEntity.badRequest().body(Message("user not found"))
 //        if (client == null)
@@ -35,8 +41,15 @@ class authController (private val clientService: ClientService){
             return ResponseEntity.badRequest().body(Message("invalid credencials"))
         }
 
+        val issuer = client.id.toString()
 
-        return ResponseEntity.ok(client)
+        val jwt = Jwts.builder()
+            .setIssuer(issuer)
+            .setExpiration(Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000 )) // = 1 day
+            .signWith(SignatureAlgorithm.HS256, "secret").compact() // .compact wraps everything and generate a string
+        return ResponseEntity.ok(jwt) //returns JWT
+
+
     }
 
 }
